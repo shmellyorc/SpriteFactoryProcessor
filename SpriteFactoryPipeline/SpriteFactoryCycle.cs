e7dfa5ac7b78227fc790f80f52b166187188c84f
shmellyorc <shmellyorc@gmail.com>
2021-09-15 12:57:26 -0700

    Update SpriteFactoryCycle.cs

--- SpriteFactoryPipeline/SpriteFactoryCycle.cs
+++ SpriteFactoryPipeline/SpriteFactoryCycle.cs
@@ -3,14 +3,16 @@ namespace StinkyEngine.Pipelines.SpriteFactoryPipeline
     public class SpriteFactoryCycle
     {
         public int[] Frames { get; set; }
-		public bool IsLooping { get; set; }
-		public float FrameDuration { get; set; }
+	    
+	public bool IsLooping { get; set; }
+	    
+	public float FrameDuration { get; set; }
 
-		public SpriteFactoryCycle(bool isLooping, int[] frames, float frameDuration)
-		{
-			IsLooping = isLooping;
-			Frames = frames;
-			FrameDuration = frameDuration;
-		}
+	public SpriteFactoryCycle(bool isLooping, int[] frames, float frameDuration)
+	{
+		IsLooping = isLooping;
+		Frames = frames;
+		FrameDuration = frameDuration;
+	}
     }
 }
