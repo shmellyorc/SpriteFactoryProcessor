08bc5afda08dc44d50db7be9a301eff2e06aefb9
shmellyorc <shmellyorc@gmail.com>
2021-09-15 13:00:20 -0700

    Update SpriteFactoryAtlas.cs

--- SpriteFactoryPipeline/SpriteFactoryAtlas.cs
+++ SpriteFactoryPipeline/SpriteFactoryAtlas.cs
@@ -5,16 +5,16 @@ namespace StinkyEngine.Pipelines.SpriteFactoryPipeline
 {
     public class SpriteFactoryAtlas
     {
-		[JsonProperty(PropertyName = "Texture")]
+	[JsonProperty(PropertyName = "Texture")]
         public string Path { get; set; }
 
-		[JsonIgnore]
-		public Texture2D Texture { get; set; }
+	[JsonIgnore]
+	public Texture2D Texture { get; set; }
 
-		[JsonProperty(PropertyName = "regionWidth")]
-		public int Width { get; set; }
+	[JsonProperty(PropertyName = "regionWidth")]
+	public int Width { get; set; }
 
-		[JsonProperty(PropertyName = "regionHeight")]
-		public int Height { get; set; }
+	[JsonProperty(PropertyName = "regionHeight")]
+	public int Height { get; set; }
     }
 }
