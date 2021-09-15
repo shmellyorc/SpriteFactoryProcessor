3fa70c9120d5f18ac43e4fc4d6dfbd0350e60019
shmellyorc <shmellyorc@gmail.com>
2021-09-15 12:56:59 -0700

    Update SpriteFactory.cs

--- SpriteFactoryPipeline/SpriteFactory.cs
+++ SpriteFactoryPipeline/SpriteFactory.cs
@@ -4,7 +4,8 @@ namespace StinkyEngine.Pipelines.SpriteFactoryPipeline
     public class SpriteFactory
     {
         public SpriteFactoryAtlas TextureAtlas { get; set; }
-		public Dictionary<string, SpriteFactoryCycle> Cycles { get; set; } =
-			new Dictionary<string, SpriteFactoryCycle>();
+	    
+	public Dictionary<string, SpriteFactoryCycle> Cycles { get; set; } =
+		new Dictionary<string, SpriteFactoryCycle>();
     }
 }
