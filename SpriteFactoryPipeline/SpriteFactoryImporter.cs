01beccbdbbd370a51667ba437951472e10716a1e
shmellyorc <shmellyorc@gmail.com>
2021-09-15 12:56:36 -0700

    Update SpriteFactoryImporter.cs

--- SpriteFactoryPipeline/SpriteFactoryImporter.cs
+++ SpriteFactoryPipeline/SpriteFactoryImporter.cs
@@ -6,16 +6,16 @@ using Newtonsoft.Json;
 namespace StinkyEngine.Pipelines.SpriteFactoryPipeline
 {
 	[ContentImporter(".sf", DefaultProcessor = "SpriteFactoryProcessor", DisplayName = "Sprite Factory Importer")]
-    public class SpriteFactoryImporter : ContentImporter<SpriteFactory>
-    {
-        public override SpriteFactory Import(string filename, ContentImporterContext context)
+	public class SpriteFactoryImporter : ContentImporter<SpriteFactory>
 	{
-		context.Logger.LogMessage($"Importing Sprite Factory: {filename}");
+		public override SpriteFactory Import(string filename, ContentImporterContext context)
+		{
+			context.Logger.LogMessage($"Importing Sprite Factory: {filename}");
 
-		var data = File.ReadAllText(filename);
-		return JsonConvert.DeserializeObject<SpriteFactory>(data);
+			var data = File.ReadAllText(filename);
+			return JsonConvert.DeserializeObject<SpriteFactory>(data);
+		}
 	}
-    }
 }
 
 		
